372ef9a1da9ab4d87cf8f42210441369ebd9f022
jshields123 <jackshields@live.co.uk>
2024-11-10 12:34:32 +1100

    added in bio content and about page and responsiveness

--- app/components/Paragraph/Paragraph.jsx
+++ app/components/Paragraph/Paragraph.jsx
@@ -1,5 +1,4 @@
 import styles from "./Paragraph.module.css";
-import { PARAGRAPH_CONTENT } from "./Paragraph.constants";
 
 const Paragraph = ({ text }) => {
   return <p className={styles.paragraph}>{text}</p>;
